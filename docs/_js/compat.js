494987b6ba8db10f45d3fecc7dbde5d4cb5c0824
AShiningRay <genesis45.ppc@gmail.com>
2023-06-07 13:10:45 -0300

    Add support for filtering compatibility table entries by status
    Previously those status buttons were just for show, let's add some
    functionality to them and allow users to filter entries based on
    the toggled status buttons.

--- docs/_js/compat.js
+++ docs/_js/compat.js
@@ -4,6 +4,16 @@ var searchready = false;
 var rowdata;
 /* Compatibility chart colors */
 var colors=['#81d41a','#729fcf','#ffff38', '#ff8000', '#ff0000'];
+/* Amount of apps in each compatibility state */
+        /* ['Perfect','Minor issues','Playable','Ingame','Not booting'] */
+var values=[0, 0, 0, 0, 0];
+
+/* Variables to filter entries by compatibility state */
+var perfect_enabled = true;
+var minor_issue_enabled = true;
+var playable_enabled = true;
+var ingame_enabled = true;
+var not_booting_enabled = true;
 
 /* 
  * Once the window loads, get the csv with the compatibility list and prepare
@@ -15,16 +25,12 @@ window.onload = function () {
 
 /* Biggest function of the entire website, tasked of building the compatibility list */
 function readCSV() {
-  /* Amount of apps in each compatibility state */
-          /* ['Perfect','Minor issues','Playable','Ingame','Not booting'] */
-  var values=[0, 0, 0, 0, 0];
-
   /* Fetch the csv file and begin processing it */
   fetch('../compat_data/FreeJ2ME Compatibility.csv').then(response => response.text()) 
   .then(csvFile => {
-      /* Split csv lines as rows */
-      rowdata = csvFile.split('\n');
-      generateCompatData(values);
+    /* Split csv lines as rows */
+    rowdata = csvFile.split('\n');
+    generateCompatData();
 
     
     /* Add data to the buttons and app counter */
@@ -76,56 +82,68 @@ function readCSV() {
 }
 
 /* Helper function to generate the compatibility date separate from teh main csv function */
-function generateCompatData(values) {
+function generateCompatData() {
   var statcolor = '', maindivname='', elem_bordercolor='';
   var compat_table = document.getElementById('compat_table');
+  var add_entry = false;
+
+  /* Empty the compat_table contents beforehand, useful in case of a regen call. */
+  compat_table.innerHTML = '';
 
   /* For each entry on the compatibility list: */
   for (row of rowdata) {
+
     /* Last row of csv is always empty, so treat that case */
     if(row.length > 0) {
       /* Columns are separated by '|' in the csv */
       columndata = row.split('|');
+
+      /* Filter entry based on enabled compatibility states */
+      add_entry = false;
+
       /* TODO: Improve the text detection here, as any minor deviation can make a pass fail */
       switch(columndata[2]) {
         case 'Perfect':
-          values[0] +=1;
           statcolor = 'style="background-color:' + colors[0] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[0] + ';"';
+          if(perfect_enabled) { add_entry = true; values[0] +=1; }
           break;
         case 'Minor issues':
-          values[1] +=1;
           statcolor = 'style="background-color:' + colors[1] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[1] + ';"';
+          if(minor_issue_enabled) { add_entry = true; values[1] +=1; }
           break;
         case 'Playable':
-          values[2] +=1;
           statcolor = 'style="background-color:' + colors[2] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[2] + ';"';
+          if(playable_enabled) { add_entry = true; values[2] +=1; }
           break;
         case 'Ingame':
-          values[3] +=1;
           statcolor = 'style="background-color:' + colors[3] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[3] + ';"';
+          if(ingame_enabled) { add_entry = true; values[3] +=1; }
           break;
         case 'Not booting':
-          values[4] +=1;
           statcolor = 'style="background-color:' + colors[4] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[4] + ';"';
+          if(not_booting_enabled) { add_entry = true; values[4] +=1; }
           break;
         default: /* Skip any invalid entries */
           continue;
       }
       
-      /* Inserts each row's data into the expected div */
-      if(((values[0]+values[1]+values[2]+values[3]+values[4])) %2 == 0) {
-        maindivname = 'id="compat_entry0"';
+      if(add_entry) {
+        /* Inserts each row's data into the expected div */
+        if(((values[0]+values[1]+values[2]+values[3]+values[4])) %2 == 0) {
+          maindivname = 'id="compat_entry0"';
+        }
+        else {
+          maindivname = 'id="compat_entry1"';
+        }
+        
+        addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
       }
-      else {
-        maindivname = 'id="compat_entry1"';
-      }
-      
-      addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
+
     }
   }
 }
@@ -137,57 +155,66 @@ function searchApp() {
     var statcolor = '', elem_bordercolor='', animdelay='';
     var i = 0, values = [0, 0, 0, 0, 0];
     var compat_table = document.getElementById('compat_table');
+    var add_entry = false;
 
     /* Empty the compatibility list in order to add only relevant results */
     compat_table.innerHTML = '';
 
     /* Turns out using includes() to match strings allows us to not regen the whole table with an empty string */
     for (row of rowdata) {
-      columndata = row.split('|');
+      add_entry = false;
+        /* Last row of csv is always empty, so treat that case */
+      if(row.length > 0) {
+        columndata = row.split('|');
 
-      if(columndata[0].toLowerCase().includes(searchcontents)) {
-        i+=1;
+        if(columndata[0].toLowerCase().includes(searchcontents)) {
+          i+=1;
 
-        /* TODO: Improve the text detection here, as any minor deviation can make a pass fail */
-        switch(columndata[2]) {
-          case 'Perfect':
-            values[0] +=1;
-            statcolor = 'style="background-color:' + colors[0] + ';"';
-            elem_bordercolor = 'style="border-color:' + colors[0] + ';"';
-            break;
-          case 'Minor issues':
-            values[1] +=1;
-            statcolor = 'style="background-color:' + colors[1] + ';"';
-            elem_bordercolor = 'style="border-color:' + colors[1] + ';"';
-            break;
-          case 'Playable':
-            values[2] +=1;
-            statcolor = 'style="background-color:' + colors[2] + ';"';
-            elem_bordercolor = 'style="border-color:' + colors[2] + ';"';
-            break;
-          case 'Ingame':
-            values[3] +=1;
-            statcolor = 'style="background-color:' + colors[3] + ';"';
-            elem_bordercolor = 'style="border-color:' + colors[3] + ';"';
-            break;
-          case 'Not booting':
-            values[4] +=1;
-            statcolor = 'style="background-color:' + colors[4] + ';"';
-            elem_bordercolor = 'style="border-color:' + colors[4] + ';"';
-            break;
-          default: /* Skip any invalid entries */
-            continue;
-        }
+          /* TODO: Improve the text detection here, as any minor deviation can make a pass fail */
+          switch(columndata[2]) {
+            case 'Perfect':
+              statcolor = 'style="background-color:' + colors[0] + ';"';
+              elem_bordercolor = 'style="border-color:' + colors[0] + ';"';
+              if(perfect_enabled) { add_entry = true; values[0] +=1; }
+              break;
+            case 'Minor issues':
+              statcolor = 'style="background-color:' + colors[1] + ';"';
+              elem_bordercolor = 'style="border-color:' + colors[1] + ';"';
+              if(minor_issue_enabled) { add_entry = true; values[1] +=1; }
+              break;
+            case 'Playable':
+              statcolor = 'style="background-color:' + colors[2] + ';"';
+              elem_bordercolor = 'style="border-color:' + colors[2] + ';"';
+              if(playable_enabled) { add_entry = true; values[2] +=1; }
+              break;
+            case 'Ingame':
+              statcolor = 'style="background-color:' + colors[3] + ';"';
+              elem_bordercolor = 'style="border-color:' + colors[3] + ';"';
+              if(ingame_enabled) { add_entry = true; values[3] +=1; }
+              break;
+            case 'Not booting':
+              statcolor = 'style="background-color:' + colors[4] + ';"';
+              elem_bordercolor = 'style="border-color:' + colors[4] + ';"';
+              if(not_booting_enabled) { add_entry = true; values[4] +=1; }
+              break;
+            default: /* Skip any invalid entries */
+              continue;
+          }
 
-        /* Inserts each row's data into the expected div */
-        if(i%2 == 0) {
-          maindivname = 'id="compat_entry0"';
+          /* Only filter by compat status when the search string is empty. */
+          if(add_entry || searchcontents !== '') {
+            /* Inserts each row's data into the expected div */
+            if(i%2 == 0) {
+              maindivname = 'id="compat_entry0"';
+            }
+            else {
+              maindivname = 'id="compat_entry1"';
+            }
+            
+            addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
+          }
+
         }
-        else {
-          maindivname = 'id="compat_entry1"';
-        }
-        
-        addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
       }
     }
   }
@@ -204,4 +231,57 @@ function addElements(compat_table, columndata, compat_table, maindivname, elem_b
       <div id="entryupd"><div id="extrabg">'  + columndata[4] + '</div></div>\
       <div id="entrymd5"><div id="extrabg">'  + columndata[5] + '</div></div>\
     </div>';
+}
+
+function toggleStatus(status) {
+  if (status === 'perfect') {
+    
+    if(perfect_enabled) {
+      document.getElementById('b_perfect').style.backgroundColor = 'transparent';
+    } else {
+      document.getElementById('b_perfect').style.backgroundColor = colors[0];
+      document.getElementById('b_perfect').style.borderColor = colors[0];
+    }
+    perfect_enabled = !perfect_enabled;
+
+  } else if (status === 'minor_issue') {
+    
+    if(minor_issue_enabled) {
+      document.getElementById('b_minor_issue').style.backgroundColor = 'transparent';
+    } else {
+      document.getElementById('b_minor_issue').style.backgroundColor = colors[1];
+    }
+    minor_issue_enabled = !minor_issue_enabled;
+
+  } else if (status === 'playable') {
+    
+    if(playable_enabled) {
+      document.getElementById('b_playable').style.backgroundColor = 'transparent';
+    } else {
+      document.getElementById('b_playable').style.backgroundColor = colors[2];
+    }
+    playable_enabled = !playable_enabled;
+
+  } else if (status === 'ingame') {
+    
+    if(ingame_enabled) {
+      document.getElementById('b_ingame').style.backgroundColor = 'transparent';
+    } else {
+      document.getElementById('b_ingame').style.backgroundColor = colors[3];
+    }
+    ingame_enabled = !ingame_enabled;
+
+  } else if (status === 'not_booting') {
+    
+    if(not_booting_enabled) {
+      document.getElementById('b_not_booting').style.backgroundColor = 'transparent';
+    } else {
+      document.getElementById('b_not_booting').style.backgroundColor = colors[4];
+    }
+    not_booting_enabled = !not_booting_enabled;
+
+  }
+
+  /* Update the compatibility list with the filters */
+  generateCompatData();
 }
