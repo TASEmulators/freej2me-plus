9c54e61b545f23bb0cc558e882d4fc303984f6e1
AShiningRay <genesis45.ppc@gmail.com>
2023-06-12 22:20:10 -0300

    docs: Improve the website and add new entries to the compat list
    JavaScript logic got a major overhaul and now searching for
    entries or filtering by compatibility states is much faster than
    before. The first webpage load is also significantly faster since
    the compatibility list does not rely on multiple innerHTML calls
    anymore.

--- docs/_js/compat.js
+++ docs/_js/compat.js
@@ -7,6 +7,7 @@ var colors=['#81d41a','#729fcf','#ffff38', '#ff8000', '#ff0000'];
 /* Amount of apps in each compatibility state */
         /* ['Perfect','Minor issues','Playable','Ingame','Not booting'] */
 var values=[0, 0, 0, 0, 0];
+var total_apps = 0;
 
 /* Variables to filter entries by compatibility state */
 var perfect_enabled = true;
@@ -36,7 +37,8 @@ function readCSV() {
     /* Add data to the buttons and app counter */
   
     /* A "for" loop isn't really needed here since we'll only have 5 status categories */
-    document.getElementById('total_apps').textContent += (values[0]+values[1]+values[2]+values[3]+values[4]);
+    total_apps = values[0]+values[1]+values[2]+values[3]+values[4];
+    document.getElementById('total_apps').textContent += total_apps;
   
     document.getElementById('b_perfect').textContent += values[0];
     document.getElementById('b_minor_issue').textContent += values[1];
@@ -85,7 +87,8 @@ function readCSV() {
 function generateCompatData() {
   var statcolor = '', maindivname='', elem_bordercolor='';
   var compat_table = document.getElementById('compat_table');
-  var add_entry = false;
+  var i = 0;
+  var temp_elements = '';
 
   /* Empty the compat_table contents beforehand, useful in case of a regen call. */
   compat_table.innerHTML = '';
@@ -98,139 +101,120 @@ function generateCompatData() {
       /* Columns are separated by '|' in the csv */
       columndata = row.split('|');
 
-      /* Filter entry based on enabled compatibility states */
-      add_entry = false;
-
       /* TODO: Improve the text detection here, as any minor deviation can make a pass fail */
       switch(columndata[2]) {
         case 'Perfect':
           statcolor = 'style="background-color:' + colors[0] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[0] + ';"';
-          if(perfect_enabled) { add_entry = true; values[0] +=1; }
+          values[0] +=1;
           break;
         case 'Minor issues':
           statcolor = 'style="background-color:' + colors[1] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[1] + ';"';
-          if(minor_issue_enabled) { add_entry = true; values[1] +=1; }
+          values[1] +=1;
           break;
         case 'Playable':
           statcolor = 'style="background-color:' + colors[2] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[2] + ';"';
-          if(playable_enabled) { add_entry = true; values[2] +=1; }
+          values[2] +=1;
           break;
         case 'Ingame':
           statcolor = 'style="background-color:' + colors[3] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[3] + ';"';
-          if(ingame_enabled) { add_entry = true; values[3] +=1; }
+          values[3] +=1;
           break;
         case 'Not booting':
           statcolor = 'style="background-color:' + colors[4] + ';"';
           elem_bordercolor = 'style="border-color:' + colors[4] + ';"';
-          if(not_booting_enabled) { add_entry = true; values[4] +=1; }
+          values[4] +=1;
           break;
         default: /* Skip any invalid entries */
           continue;
       }
       
-      if(add_entry) {
-        /* Inserts each row's data into the expected div */
-        if(((values[0]+values[1]+values[2]+values[3]+values[4])) %2 == 0) {
-          maindivname = 'id="compat_entry0"';
-        }
-        else {
-          maindivname = 'id="compat_entry1"';
-        }
-        
-        addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
-      }
+      /* Inserts each row's data into the expected div */
+      maindivname = 'id="compat_entry' + i + '"';
 
+      temp_elements += '\
+      <div class="compat_entry" ' + maindivname + elem_bordercolor +  '>' + '\n \
+        <div id="entryname">' + columndata[0] + '</div>\
+        <div id="entryres">'  + columndata[1] + '</div>\
+        <div id="entrystat"><div id="statbg" ' + statcolor + '>' + columndata[2] + '</div></div>\
+        <div id="entrydesc">' + columndata[3] + '</div>\
+        <div id="entryupd"><div id="extrabg">'  + columndata[4] + '</div></div>\
+        <div id="entrymd5"><div id="extrabg">'  + columndata[5] + '</div></div>\
+      </div>';
+
+      i+=1;
     }
   }
+
+  /* 
+   * Only effectively add all elements to the page after parsing everything. This avoids multiple 
+   * costly calls to concatenate text into 'compat_table.innerHTML'. 
+   */
+  compat_table.innerHTML += temp_elements;
 }
 
 /* Function that implements the search function for the compatibility list */
 function searchApp() {
   if(searchready) {
     var searchcontents = document.getElementById('appsearch').value.toLowerCase();
-    var statcolor = '', elem_bordercolor='', animdelay='';
-    var i = 0, values = [0, 0, 0, 0, 0];
-    var compat_table = document.getElementById('compat_table');
-    var add_entry = false;
+    var i;
+    var compat_entry, entryname;
 
-    /* Empty the compatibility list in order to add only relevant results */
-    compat_table.innerHTML = '';
+    /* 
+     * No need to re-add elements to DOM, just hide everything that doesn't include the search string
+     * and show everything that includes it. It's much faster and also shows all elements if the string
+     * is empty.
+     */
+    for (i = 0; i < total_apps; i++) {
+      compat_entry = document.getElementById(`compat_entry${i}`);
+      /* The name of any given entry is on the very first element of compat_entry */
+      entryname = compat_entry.children[0];
 
-    /* Turns out using includes() to match strings allows us to not regen the whole table with an empty string */
-    for (row of rowdata) {
-      add_entry = false;
-        /* Last row of csv is always empty, so treat that case */
-      if(row.length > 0) {
-        columndata = row.split('|');
-
-        if(columndata[0].toLowerCase().includes(searchcontents)) {
-          i+=1;
-
-          /* TODO: Improve the text detection here, as any minor deviation can make a pass fail */
-          switch(columndata[2]) {
-            case 'Perfect':
-              statcolor = 'style="background-color:' + colors[0] + ';"';
-              elem_bordercolor = 'style="border-color:' + colors[0] + ';"';
-              if(perfect_enabled) { add_entry = true; values[0] +=1; }
-              break;
-            case 'Minor issues':
-              statcolor = 'style="background-color:' + colors[1] + ';"';
-              elem_bordercolor = 'style="border-color:' + colors[1] + ';"';
-              if(minor_issue_enabled) { add_entry = true; values[1] +=1; }
-              break;
-            case 'Playable':
-              statcolor = 'style="background-color:' + colors[2] + ';"';
-              elem_bordercolor = 'style="border-color:' + colors[2] + ';"';
-              if(playable_enabled) { add_entry = true; values[2] +=1; }
-              break;
-            case 'Ingame':
-              statcolor = 'style="background-color:' + colors[3] + ';"';
-              elem_bordercolor = 'style="border-color:' + colors[3] + ';"';
-              if(ingame_enabled) { add_entry = true; values[3] +=1; }
-              break;
-            case 'Not booting':
-              statcolor = 'style="background-color:' + colors[4] + ';"';
-              elem_bordercolor = 'style="border-color:' + colors[4] + ';"';
-              if(not_booting_enabled) { add_entry = true; values[4] +=1; }
-              break;
-            default: /* Skip any invalid entries */
-              continue;
-          }
-
-          /* Only filter by compat status when the search string is empty. */
-          if(add_entry || searchcontents !== '') {
-            /* Inserts each row's data into the expected div */
-            if(i%2 == 0) {
-              maindivname = 'id="compat_entry0"';
-            }
-            else {
-              maindivname = 'id="compat_entry1"';
-            }
-            
-            addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor);
-          }
-
-        }
+      if(entryname.innerHTML.toLowerCase().includes(searchcontents)) {
+        compat_entry.style.display = "flex";
+      }
+      else {
+        compat_entry.style.display = "none";
       }
     }
   }
 }
 
-/* Helper function to add elements to the compat table DOM */
-function addElements(compat_table, columndata, compat_table, maindivname, elem_bordercolor, statcolor) {
-  compat_table.innerHTML += '\
-    <div class="compat_entry" ' + maindivname + elem_bordercolor +  '>' + '\n \
-      <div id="entryname">' + columndata[0] + '</div>\
-      <div id="entryres">'  + columndata[1] + '</div>\
-      <div id="entrystat"><div id="statbg" ' + statcolor + '>' + columndata[2] + '</div></div>\
-      <div id="entrydesc">' + columndata[3] + '</div>\
-      <div id="entryupd"><div id="extrabg">'  + columndata[4] + '</div></div>\
-      <div id="entrymd5"><div id="extrabg">'  + columndata[5] + '</div></div>\
-    </div>';
+function updateCompatState() {
+  var i;
+  var compat_entry, entrystat;
+
+  for (i = 0; i < total_apps; i++) {
+    compat_entry = document.getElementById(`compat_entry${i}`);
+
+    /* 
+     * The compat status of any given entry is on the second element of compat_entry,
+     * but it has a div inside of it, so the text is even further in.
+     */
+    entrystat = compat_entry.children[2].children[0];
+
+    /* Make all entries begin as 'display: none' to significantly shorten the conditionals below. */
+    compat_entry.style.display = "none";
+
+    if(entrystat.textContent.toLowerCase() === "perfect" && perfect_enabled) {
+        compat_entry.style.display = "flex";
+
+    } else if(entrystat.textContent.toLowerCase() === "minor issues" && minor_issue_enabled) {
+        compat_entry.style.display = "flex";
+
+    } else if(entrystat.textContent.toLowerCase() === "playable" && playable_enabled) {
+        compat_entry.style.display = "flex";
+
+    } else if(entrystat.textContent.toLowerCase() === "ingame" && ingame_enabled) {
+        compat_entry.style.display = "flex";
+
+    } else if(entrystat.textContent.toLowerCase() === "not booting" && not_booting_enabled) {
+        compat_entry.style.display = "flex";
+    }
+  }
 }
 
 function toggleStatus(status) {
@@ -283,5 +267,5 @@ function toggleStatus(status) {
   }
 
   /* Update the compatibility list with the filters */
-  generateCompatData();
+  updateCompatState();
 }
