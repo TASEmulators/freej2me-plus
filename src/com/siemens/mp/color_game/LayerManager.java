c73b8a6a5d08a978ba17e78476f211042fa572c1
AShiningRay <genesis45.ppc@gmail.com>
2024-11-08 23:13:17 -0300

    siemens: color_game: Do not extend lcdui.game classes directly
    Just implement them nearly 1:1 compared to lcdui instead. Java
    doesn't like it when jars attempt to load both lcdui and siemens
    classes at the same time it seems... allows Wizardry 1 (Mobile Scope AG)
    to head ingame again.

--- src/com/siemens/mp/color_game/LayerManager.java
+++ src/com/siemens/mp/color_game/LayerManager.java
@@ -14,19 +14,107 @@
 	You should have received a copy of the GNU General Public License
 	along with FreeJ2ME.  If not, see http://www.gnu.org/licenses/
 */
-
-
 package com.siemens.mp.color_game;
 
-import java.util.Vector;
-
-import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Graphics;
 
-import org.recompile.mobile.Mobile;
-import org.recompile.mobile.PlatformGraphics;
+public class LayerManager
+{
 
-public class LayerManager extends javax.microedition.lcdui.game.LayerManager 
-{ 
-	public LayerManager() { super(); }
+	private Layer component[] = new Layer[4];
+
+	private int layers;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+
+
+	public LayerManager() { setViewWindow(0, 0, Integer.MAX_VALUE, Integer.MAX_VALUE); }
+
+	// This is just an insert() call, but with the last layer pos as the index.
+	public void append(Layer l) { insert(l, layers); }
+
+	public Layer getLayerAt(int index) 
+	{ 
+		if ((index < 0) || (index >= layers)) { throw new IndexOutOfBoundsException(); }
+
+		return component[index];
+	}
+
+	public int getSize() { return layers; }
+
+	public void insert(Layer l, int index) 
+	{ 
+		if ((index < 0) || (index > layers) || (exist(l) && (index >= layers))) { throw new IndexOutOfBoundsException(); }
+	
+		remove(l);
+
+		if (layers == component.length)
+		{
+			Layer newcomponents[] = new Layer[layers + 4];
+			System.arraycopy(component, 0, newcomponents, 0, layers);
+			System.arraycopy(component, index, newcomponents, index + 1, layers - index);
+			component = newcomponents;
+		}
+		else { System.arraycopy(component, index, component, index + 1, layers - index); }
+
+		component[index] = l;
+		layers++;
+	}
+
+	public void paint(Graphics g, int xdest, int ydest)
+	{
+		int cx = g.getClipX();
+		int cy = g.getClipY();
+		int cw = g.getClipWidth();
+		int ch = g.getClipHeight();
+
+		g.translate(xdest - x, ydest - y);
+		// set the clip to view window
+		g.clipRect(x, y, width, height);
+
+		for (int i = layers-1; i >= 0; i--)
+		{
+			Layer comp = component[i];
+			if (component[i].visible) { component[i].paint(g); }
+		}
+
+		g.translate(-xdest + x, -ydest + y);
+		g.setClip(cx, cy, cw, ch);
+	}
+
+	public void remove(Layer l) 
+	{ 
+		if (l == null) { throw new NullPointerException(); }
+
+		for (int i = layers-1; i >= 0; i--)
+		{
+			if (component[i] == l) 
+			{
+				System.arraycopy(component, i + 1, component, i, layers - i - 1);
+				component[--layers] = null;
+			}
+		}
+	}
+
+	private boolean exist(Layer l)
+	{
+		if (l == null) { return false; }
+
+		for (int i = layers; --i >= 0; ) { if (component[i] == l) { return true; } }
+		return false;
+	}
+
+	public void setViewWindow(int wx, int wy, int wwidth, int wheight)
+	{
+		if (width < 0 || height < 0) { throw new IllegalArgumentException(); }
+
+		x = wx;
+		y = wy;
+		width = wwidth;
+		height = wheight;
+	}
+
+	public void removeLayer(com.siemens.mp.misc.NativeMem NativeMemoryTable, Layer l) { System.out.println("Siemens: color_game.LayerManager: removeLayer()"); } // TODO
 }
--- src/com/siemens/mp/color_game/TiledLayer.java
+++ src/com/siemens/mp/color_game/TiledLayer.java
@@ -14,12 +14,244 @@
 	You should have received a copy of the GNU General Public License
 	along with FreeJ2ME.  If not, see http://www.gnu.org/licenses/
 */
-
 package com.siemens.mp.color_game;
 
 import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Graphics;
 
-public class TiledLayer extends javax.microedition.lcdui.game.TiledLayer 
-{ 
-	public TiledLayer(int colsw, int rowsh, Image baseimage, int tileWidth, int tileHeight) { super(colsw, rowsh, baseimage, tileWidth, tileHeight); }
+import java.util.Arrays;
+
+public class TiledLayer extends Layer 
+{
+
+	protected Image image;
+	private int rows;
+	private int cols;
+	private int tileHeight;
+	private int tileWidth;
+	
+	private int numberOfTiles;
+	protected int[] tileSetX;
+	protected int[] tileSetY;
+	private int[] animatedTiles;
+	private int animatedTileCount = 0;
+
+	private int[][] tiles;
+
+	public TiledLayer(int colsw, int rowsh, Image baseimage, int tileWidth, int tileHeight) 
+	{
+		super(colsw < 1 || tileWidth < 1 ? -1 : colsw * tileWidth, rowsh < 1 || tileHeight < 1 ? -1 : rowsh * tileHeight);
+
+		if (((baseimage.getWidth() % tileWidth) != 0) || ((baseimage.getHeight() % tileHeight) != 0)) { throw new IllegalArgumentException(); }
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+		this.cols = colsw;
+		this.rows = rowsh;
+
+		x = 0;
+		y = 0;
+		width = tileWidth*cols;
+		height = tileHeight*rows;
+
+		tiles = new int[rowsh][colsw];
+
+		final int noOfFrames = (baseimage.getWidth() / tileWidth) * (baseimage.getHeight() / tileHeight);
+		createStaticSet(baseimage, noOfFrames + 1, tileWidth, tileHeight, true);
+	}
+
+	public int createAnimatedTile(int staticTileIndex) 
+	{
+		if (staticTileIndex < 0 || staticTileIndex >= numberOfTiles) { throw new IndexOutOfBoundsException(); }
+
+		if (animatedTiles == null) 
+		{
+			animatedTiles = new int[4];
+			animatedTileCount = 1;
+		} 
+		else if (animatedTileCount == animatedTiles.length) 
+		{
+			// AnimatedTiles limit has been reached, we will need to increase its size
+			int newAnimatedTiles[] = new int[animatedTiles.length * 2];
+			System.arraycopy(animatedTiles, 0, newAnimatedTiles, 0, animatedTiles.length);
+			animatedTiles = newAnimatedTiles;
+		}
+
+		animatedTiles[animatedTileCount] = staticTileIndex;
+		animatedTileCount++;
+		return (-(animatedTileCount - 1));
+	}
+
+	public void setAnimatedTile(int animatedTileIndex, int staticTileIndex) 
+	{
+		if (staticTileIndex < 0 || staticTileIndex >= numberOfTiles) { throw new IndexOutOfBoundsException(); }
+		
+		animatedTileIndex = -animatedTileIndex;
+		if (animatedTiles == null || animatedTileIndex <= 0 || animatedTileIndex >= animatedTileCount) { throw new IndexOutOfBoundsException(); }
+
+		animatedTiles[animatedTileIndex] = staticTileIndex;
+	}
+
+	public int getAnimatedTile(int animatedTileIndex) 
+	{
+		animatedTileIndex = -animatedTileIndex;
+		if (animatedTiles == null || animatedTileIndex <= 0 || animatedTileIndex >= animatedTileCount) { throw new IndexOutOfBoundsException(); }
+
+		return animatedTiles[animatedTileIndex];
+	}
+
+	public void setCell(int col, int row, int tileIndex) 
+	{
+		if (col < 0 || col >= this.cols || row < 0 || row >= this.rows) { throw new IndexOutOfBoundsException(); }
+
+		if (tileIndex > 0) { if (tileIndex >= numberOfTiles) { throw new IndexOutOfBoundsException(); } } 
+		else if (tileIndex < 0) { if (animatedTiles == null || (-tileIndex) >= animatedTileCount) { throw new IndexOutOfBoundsException(); } }
+
+		tiles[row][col] = tileIndex;
+	}
+
+	public int getCell(int col, int row) 
+	{
+		if (col < 0 || col >= this.cols || row < 0 || row >= this.rows) { throw new IndexOutOfBoundsException(); }
+		return tiles[row][col];
+	}
+
+	public void fillCells(int col, int row, int numCols, int numRows, int tileIndex) 
+	{
+		if (numCols < 0 || numRows < 0) { throw new IllegalArgumentException(); }
+
+		if (col < 0 || col >= this.cols || row < 0 || row >= this.rows || col + numCols > this.cols || row + numRows > this.rows)  { throw new IndexOutOfBoundsException(); }
+
+		if (tileIndex > 0) { if (tileIndex >= numberOfTiles) { throw new IndexOutOfBoundsException(); } } 
+		else if (tileIndex < 0) { if (animatedTiles == null || (-tileIndex) >= animatedTileCount) { throw new IndexOutOfBoundsException(); } }
+
+		for (int rowCount = row; rowCount < row + numRows; rowCount++) { Arrays.fill(tiles[rowCount], col, col + numCols, tileIndex); }
+	}
+
+	public final int getCellWidth() { return tileWidth; }
+
+	public final int getCellHeight() { return tileHeight; }
+
+	public final int getColumns() { return cols; }
+
+	public final int getRows() { return rows; }
+
+	public void setStaticTileSet(Image baseimage, int tileWidth, int tileHeight) 
+	{
+		if (tileWidth < 1 || tileHeight < 1 || ((baseimage.getWidth() % tileWidth) != 0) || ((baseimage.getHeight() % tileHeight) != 0)) 
+			{ throw new IllegalArgumentException(); }
+
+		setWidth(cols * tileWidth);
+		setHeight(rows * tileHeight);
+
+		int noOfFrames = (baseimage.getWidth() / tileWidth) * (baseimage.getHeight() / tileHeight);
+
+		// the zero index is left empty for transparent tiles
+		// so it is passed in createStaticSet as noOfFrames + 1
+		if (noOfFrames >= (numberOfTiles - 1)) { createStaticSet(baseimage, noOfFrames + 1, tileWidth, tileHeight, true); } 
+		else { createStaticSet(baseimage, noOfFrames + 1, tileWidth, tileHeight, false); }
+	}
+
+	@Override
+	public final void paint(Graphics g) 
+	{
+		if (g == null) { throw new NullPointerException(); }
+
+		if (visible) 
+		{
+			int startColumn = 0;
+			int endColumn = this.cols;
+			int startRow = 0;
+			int endRow = this.rows;
+
+			// calculate the number of columns left of the clip
+			int number = (g.getClipX() - this.x) / tileWidth;
+			if (number > 0) { startColumn = number; }
+
+			// calculate the number of columns right of the clip
+			int endX = this.x + (this.cols * tileWidth);
+			int endClipX = g.getClipX() + g.getClipWidth();
+			number = (endX - endClipX) / tileWidth;
+			if (number > 0) { endColumn -= number; }
+
+			// calculate the number of rows above the clip
+			number = (g.getClipY() - this.y) / tileHeight;
+			if (number > 0) { startRow = number; }
+
+			// calculate the number of rows below the clip
+			int endY = this.y + (this.rows * tileHeight);
+			int endClipY = g.getClipY() + g.getClipHeight();
+			number = (endY - endClipY) / tileHeight;
+			if (number > 0) { endRow -= number; }
+
+			// paint all visible cells
+			int tileIndex = 0;
+
+			// y-coordinate
+			int ty = this.y + (startRow * tileHeight);
+
+			for (int row = startRow; row < endRow; row++, ty += tileHeight) 
+			{
+				// reset the x-coordinate at the beginning of every row
+				// x-coordinate to draw tile into
+				int tx = this.x + (startColumn * tileWidth);
+				for (int column = startColumn; column < endColumn; column++, tx += tileWidth) 
+				{
+					tileIndex = tiles[row][column];
+					// check the indices
+					// if animated get the corresponding
+					// static index from animatedTiles table
+
+					// tileIndex = 0 is a transparent tile
+					if (tileIndex == 0) { continue; } 
+					else if (tileIndex < 0) { tileIndex = getAnimatedTile(tileIndex); }
+
+					g.drawRegion(image, tileSetX[tileIndex], tileSetY[tileIndex], tileWidth, tileHeight, Sprite.TRANS_NONE, tx, ty, Graphics.TOP | Graphics.LEFT);
+				}
+			}
+		}
+	}
+
+	private void createStaticSet(Image baseImage, int noOfFrames, int tileWidth, int tileHeight, boolean maintainIndices) 
+	{
+		// JBenchmark 2 uses this, and the rewrite didn't seem to have broken it, still renders as expected
+		//System.out.println("Created StaticTileSet!");
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+	
+		final int imageW = baseImage.getWidth();
+		final int imageH = baseImage.getHeight();
+	
+		this.image = baseImage;
+		this.numberOfTiles = noOfFrames;
+		this.tileSetX = new int[numberOfTiles];
+		this.tileSetY = new int[numberOfTiles];
+	
+		if (!maintainIndices) 
+		{
+			/* 
+			 * Since we don't have to maintain Indices, initialize the TileMatrix with where all
+			 * indices will be zero, then delete any animated tiles.
+			 */
+			for (int row = 0; row < tiles.length; row++) { Arrays.fill(tiles[row], 0); }
+			animatedTiles = null;
+		}
+	
+		// Now we can start actually adding tiles to the tile matrix.
+		populateTileCoordinates(imageW, imageH);
+	}
+	
+	private void populateTileCoordinates(int imageWidth, int imageHeight) 
+	{
+		int currentTile = 1;
+	
+		for (int y = 0; y < imageHeight; y += tileHeight) 
+		{
+			for (int x = 0; x < imageWidth; x += tileWidth) 
+			{
+				tileSetX[currentTile] = x;
+				tileSetY[currentTile] = y;
+				currentTile++;
+			}
+		}
+	}
 }
