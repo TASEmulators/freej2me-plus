aa9d5aec7432cc39b403e0983c74aca7f2dfdaf6
AShiningRay <genesis45.ppc@gmail.com>
2024-11-15 17:43:56 -0300

    MP3Player: Remove JavaLayerError/s
    These two were mostly useless, and their functionality could easily
    be translated directly onto BitstreamErrors and DecoderErrors.

--- src/javax/microedition/media/javazoom/jl/decoder/BitstreamErrors.java
+++ src/javax/microedition/media/javazoom/jl/decoder/BitstreamErrors.java
@@ -31,38 +31,38 @@ package javazoom.jl.decoder;
  * @since	0.0.6
  */
 
-public interface BitstreamErrors extends JavaLayerErrors
+public interface BitstreamErrors
 {
 	
 	/**
 	 * An undeterminable error occurred. 
 	 */
-	static public final int UNKNOWN_ERROR = BITSTREAM_ERROR + 0;
+	static public final int UNKNOWN_ERROR = 0x100 + 0;
 	
 	/**
 	 * The header describes an unknown sample rate.
 	 */
-	static public final int UNKNOWN_SAMPLE_RATE = BITSTREAM_ERROR + 1;
+	static public final int UNKNOWN_SAMPLE_RATE = 0x100 + 1;
 
 	/**
 	 * A problem occurred reading from the stream.
 	 */
-	static public final int STREAM_ERROR = BITSTREAM_ERROR + 2;
+	static public final int STREAM_ERROR = 0x100 + 2;
 	
 	/**
 	 * The end of the stream was reached prematurely. 
 	 */
-	static public final int UNEXPECTED_EOF = BITSTREAM_ERROR + 3;
+	static public final int UNEXPECTED_EOF = 0x100 + 3;
 	
 	/**
 	 * The end of the stream was reached. 
 	 */
-	static public final int STREAM_EOF = BITSTREAM_ERROR + 4;
+	static public final int STREAM_EOF = 0x100 + 4;
 	
 	/**
 	 * Frame data are missing. 
 	 */
-	static public final int INVALIDFRAME = BITSTREAM_ERROR + 5;
+	static public final int INVALIDFRAME = 0x100 + 5;
 
 	/**
 	 * 
