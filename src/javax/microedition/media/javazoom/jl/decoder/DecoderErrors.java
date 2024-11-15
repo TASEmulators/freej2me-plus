397ba929ca936e96387579af66786dd0bb9bfcb6
AShiningRay <genesis45.ppc@gmail.com>
2024-11-15 17:43:56 -0300

    MP3Player: Remove JavaLayerError/s
    These two were mostly useless, and their functionality could easily
    be translated directly onto BitstreamErrors and DecoderErrors.

--- src/javax/microedition/media/javazoom/jl/decoder/DecoderErrors.java
+++ src/javax/microedition/media/javazoom/jl/decoder/DecoderErrors.java
@@ -27,19 +27,19 @@ package javazoom.jl.decoder;
  * 
  * @author	MDM
  */
-public interface DecoderErrors extends JavaLayerErrors
+public interface DecoderErrors
 {
 	
-	static public final int UNKNOWN_ERROR = DECODER_ERROR + 0;
+	static public final int UNKNOWN_ERROR = 0x200 + 0;
 	
 	/**
 	 * Layer not supported by the decoder. 
 	 */
-	static public final int UNSUPPORTED_LAYER = DECODER_ERROR + 1;
+	static public final int UNSUPPORTED_LAYER = 0x200 + 1;
 
     /**
 	 * Illegal allocation in subband layer. Indicates a corrupt stream.
 	 */
-	static public final int ILLEGAL_SUBBAND_ALLOCATION = DECODER_ERROR + 2;
+	static public final int ILLEGAL_SUBBAND_ALLOCATION = 0x200 + 2;
 
 }
