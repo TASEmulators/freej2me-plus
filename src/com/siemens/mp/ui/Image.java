ee78510654bebecc1b874a5c800f2edd74d25bd1
AShiningRay <genesis45.ppc@gmail.com>
2024-11-16 19:40:36 -0300

    Siemens: Stub and implement some missing classes.
    Image classes are by far the most important on this commit, and
    are mostly stubbed and/or untested as is, although the implementation
    of createImageWithoutScaling() on mp.ui.Image does make AH-1 SeaBomber
    boot, and work mostly fine from there on.

---
+++ src/com/siemens/mp/ui/Image.java
@@ -0,0 +1,110 @@
+/*
+	This file is part of FreeJ2ME.
+
+	FreeJ2ME is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	FreeJ2ME is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeJ2ME.  If not, see http://www.gnu.org/licenses/
+*/
+package com.siemens.mp.ui;
+
+import java.io.IOException;
+
+import org.recompile.mobile.Mobile;
+import org.recompile.mobile.PlatformImage;
+
+// NativeMem is a complete stub as is, so inherit javax Image directly
+public class Image extends javax.microedition.lcdui.Image 
+{
+    int width, height;
+    javax.microedition.lcdui.Image img;
+    public static final int COLOR_BMP_8BIT = 5;
+
+    protected Image() { }
+
+    Image(byte[] imageData) 
+    {
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(byte[]) not implemented");
+        // TODO
+    }
+
+    // The idea is to handle as much of this on lcdui.Image as possible
+    Image(byte[] bytes, int imageWidth, int imageHeight) throws IllegalArgumentException
+	{
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(byte[], width, height) not implemented");
+        //TODO: Might be incorrect
+		img = javax.microedition.lcdui.Image.createImage(bytes, width, height);
+        width = img.getWidth();
+        height = img.getHeight();
+	}
+
+    Image(byte[] bytes, int imageWidth, int imageHeight, boolean transparent) 
+    {
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(byte[], width, height, boolean transp) not implemented");
+        // TODO
+		//img = javax.microedition.lcdui.Image.createRGBImage(rgb, width, height, transparent);
+        //width = img.getWidth();
+        //height = img.getHeight();
+    }
+
+    Image(byte[] rgb, int width, int height, int bitmapType)
+	{
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(byte[], width, height, int type) not implemented");
+        // TODO: We should verify whether or not Alpha Processing is required here
+		//img = javax.microedition.lcdui.Image.createRGBImage(rgb, width, height, true);
+	}
+
+    Image(javax.microedition.lcdui.Image image) 
+    {
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(Image) untested");
+        img = javax.microedition.lcdui.Image.createImage(image);
+        width = img.getWidth();
+        height = img.getHeight();
+    }
+
+    Image(int imageWidth, int imageHeight)
+	{
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(width, height) untested");
+		img = javax.microedition.lcdui.Image.createImage(imageWidth, imageHeight);
+	}
+
+    Image(String name, boolean doScale) 
+    {
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "Image(String, boolean) not implemented");
+        try 
+        {
+            if(!doScale) { img = createImageWithoutScaling(name); }
+            else 
+            {
+                img = createImageWithoutScaling(name);
+                // Do Scaling
+            }
+        } catch (IOException e) { }
+    }
+
+    // AH-1 SeaBomber Siemens uses this, works.
+    public static javax.microedition.lcdui.Image createImageWithoutScaling(String name) throws IOException
+    { 
+        return javax.microedition.lcdui.Image.createImage(name);
+    }
+
+    /* This one is barely documented */
+    public static javax.microedition.lcdui.Image getNativeImage(Image img) 
+    {
+        Mobile.log(Mobile.LOG_WARNING, Image.class.getPackage().getName() + "." + Image.class.getSimpleName() + ": " + "getNativeImage(image) not implemented"); 
+        return img; 
+    }
+
+	public int getHeight() { return height; }
+
+	public int getWidth() { return width; }
+
+}
