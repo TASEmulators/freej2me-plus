c8e0e4ab56a05c45c27c9a7fab7578333d4134af
AShiningRay <genesis45.ppc@gmail.com>
2024-11-16 14:26:44 -0300

    Siemens.mp.game: Implement Sound.playTone and log getPixelBytes

--- src/com/siemens/mp/game/Sound.java
+++ src/com/siemens/mp/game/Sound.java
@@ -18,8 +18,17 @@
 package com.siemens.mp.game;
 
 import org.recompile.mobile.Mobile;
+import javax.microedition.media.Manager;
 
 public class Sound
 {
-	public static void playTone(int frequency, int time) { }
+	private static final double SEMITONE_CONST = 17.31234049066755;
+
+	public static void playTone(int frequency, int time) 
+	{ 
+		try { Manager.playTone(convertFreqToNote(frequency) , time, 100); }
+		catch (Exception e) { Mobile.log(Mobile.LOG_ERROR, Sound.class.getPackage().getName() + "." + Sound.class.getSimpleName() + ": " + "Failed to play tone."); }
+	}
+
+	private static int convertFreqToNote(int frequency) { return (int) (Math.round(Math.log((double) frequency / 8.176) * SEMITONE_CONST)); }
 }
