eacfffccca72f17812f3051b9d84894e3a607bf2
AShiningRay <genesis45.ppc@gmail.com>
2024-12-02 00:44:39 -0300

    MelodyComposer: Try to match nokia's OTA/OTT tones correctness.
    AH-1 SeaBomber has versions for both tone implementations, with
    Nokia's newest implementation being assumed to be more correct,
    as it is backed by some documentation. So tailor the sequence
    BPM calculation and speed in siemens' MelodyComposer to be closer
    to Nokia's on this game, hopefully improving other MelodyComposer
    jars as well.
    
    Note scales are now correct based on MIDI's expectations as well,
    but they still sound very different from nokia... not much that
    can be done here since Siemens is only supposed to go from
    C0 (24) to A4 (81) directly, at least if Siemens' note definitions
    and MIDI's note values are to match.

--- src/com/siemens/mp/game/MelodyComposer.java
+++ src/com/siemens/mp/game/MelodyComposer.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
@@ -274,93 +275,100 @@ public class MelodyComposer
 		}
 	}
 
-	private int convertLengthToTicks(int length)
+	private int convertLengthToTicks(int length) 
 	{
-        int ticksPerQuarterNote = 60000 / curMelody.bpm; // Adjust as necessary
-        switch (length) 
+		// Like on Nokia Sound's OTT/OTA: Base duration in ticks is 24 (e.g., Quarter Note)
+		int baseTicks = 24;
+	
+		switch (length) 
 		{
-            case TONELENGTH_1_1: return ticksPerQuarterNote;
-            case TONELENGTH_1_2: return ticksPerQuarterNote / 2;
-            case TONELENGTH_1_4: return ticksPerQuarterNote / 4;
-            case TONELENGTH_1_8: return ticksPerQuarterNote / 8;
-            case TONELENGTH_1_16: return ticksPerQuarterNote / 16;
-			case TONELENGTH_1_32: return ticksPerQuarterNote / 32;
-			case TONELENGTH_1_64: return ticksPerQuarterNote / 64;
-			case TONELENGTH_DOTTED_1_1: return (ticksPerQuarterNote * 3) / 2;
-			case TONELENGTH_DOTTED_1_2: return (ticksPerQuarterNote * 3) / 4;
-			case TONELENGTH_DOTTED_1_4: return (ticksPerQuarterNote * 3) / 8;
-			case TONELENGTH_DOTTED_1_8: return (ticksPerQuarterNote * 3) / 16;
-			case TONELENGTH_DOTTED_1_16: return (ticksPerQuarterNote * 3) / 32;
-			case TONELENGTH_DOTTED_1_32: return (ticksPerQuarterNote * 3) / 64;
-			case TONELENGTH_DOTTED_1_64: return (ticksPerQuarterNote * 3) / 128;
-            default: return 0;
-        }
-    }
+			case TONELENGTH_1_1: return baseTicks * 4;
+			case TONELENGTH_1_2: return baseTicks * 2;
+			case TONELENGTH_1_8: return baseTicks / 2;
+			case TONELENGTH_1_16: return baseTicks / 4;
+			case TONELENGTH_1_32: return baseTicks / 8;
+			case TONELENGTH_1_64: return baseTicks / 16;
+			case TONELENGTH_DOTTED_1_1: return (baseTicks * 4 * 3) / 2;
+			case TONELENGTH_DOTTED_1_2: return (baseTicks * 2 * 3) / 4;
+			case TONELENGTH_DOTTED_1_4: return (baseTicks * 3) / 2;
+			case TONELENGTH_DOTTED_1_8: return (baseTicks * 3) / 4;
+			case TONELENGTH_DOTTED_1_16: return (baseTicks * 3) / 8;
+			case TONELENGTH_DOTTED_1_32: return (baseTicks * 3) / 16;
+			case TONELENGTH_DOTTED_1_64: return (baseTicks * 3) / 32;
+			case TONELENGTH_1_4: 
+			default: // Unknown length will return the base tick duration.
+			return baseTicks;
+		}
+	}
 
+	/* 
+	 * Map Siemens' defined notes to MIDI. They actually map rather nicely with MIDI's notes from the get go, 
+	 * except that they have to be moved two octaves up in the scale, as MIDI's C0 note is 24, and not 0.
+	 */
 	private int mapToMidi(int tone) 
 	{
 		switch (tone) 
 		{
-			case TONE_C0: return 12;  // C0
-			case TONE_CIS0: return 13; // C#0
-			case TONE_D0: return 14;   // D0
-			case TONE_DIS0: return 15; // D#0
-			case TONE_E0: return 16;   // E0
-			case TONE_F0: return 17;   // F0
-			case TONE_FIS0: return 18; // F#0
-			case TONE_G0: return 19;   // G0
-			case TONE_GIS0: return 20; // G#0
-			case TONE_A0: return 21;   // A0
-			case TONE_AIS0: return 22; // A#0
-			case TONE_H0: return 23;   // B0
-			case TONE_C1: return 24;   // C1
-			case TONE_CIS1: return 25; // C#1
-			case TONE_D1: return 26;   // D1
-			case TONE_DIS1: return 27; // D#1
-			case TONE_E1: return 28;   // E1
-			case TONE_F1: return 29;   // F1
-			case TONE_FIS1: return 30; // F#1
-			case TONE_G1: return 31;   // G1
-			case TONE_GIS1: return 32; // G#1
-			case TONE_A1: return 33;   // A1
-			case TONE_AIS1: return 34; // A#1
-			case TONE_H1: return 35;   // B1
-			case TONE_C2: return 36;   // C2
-			case TONE_CIS2: return 37; // C#2
-			case TONE_D2: return 38;   // D2
-			case TONE_DIS2: return 39; // D#2
-			case TONE_E2: return 40;   // E2
-			case TONE_F2: return 41;   // F2
-			case TONE_FIS2: return 42; // F#2
-			case TONE_G2: return 43;   // G2
-			case TONE_GIS2: return 44; // G#2
-			case TONE_A2: return 45;   // A2
-			case TONE_AIS2: return 46; // A#2
-			case TONE_H2: return 47;   // B2
-			case TONE_C3: return 48;   // C3
-			case TONE_CIS3: return 49; // C#3
-			case TONE_D3: return 50;   // D3
-			case TONE_DIS3: return 51; // D#3
-			case TONE_E3: return 52;   // E3
-			case TONE_F3: return 53;   // F3
-			case TONE_FIS3: return 54; // F#3
-			case TONE_G3: return 55;   // G3
-			case TONE_GIS3: return 56; // G#3
-			case TONE_A3: return 57;   // A3
-			case TONE_AIS3: return 58; // A#3
-			case TONE_H3: return 59;   // B3
-			case TONE_C4: return 60;   // C4
-			case TONE_CIS4: return 61; // C#4
-			case TONE_D4: return 62;   // D4
-			case TONE_DIS4: return 63; // D#4
-			case TONE_E4: return 64;   // E4
-			case TONE_F4: return 65;   // F4
-			case TONE_FIS4: return 66; // F#4
-			case TONE_G4: return 67;   // G4
-			case TONE_GIS4: return 68; // G#4
-			case TONE_A4: return 69;   // A4
-			case NO_TONE: return 0;    // no tone (silence)
-			default: return -1;        // unknown tone
+			case TONE_C0:   return TONE_C0 + 24;   // C0
+			case TONE_CIS0: return TONE_CIS0 + 24; // C#0
+			case TONE_D0:   return TONE_D0 + 24;   // D0
+			case TONE_DIS0: return TONE_DIS0 + 24; // D#0
+			case TONE_E0:   return TONE_E0 + 24;   // E0
+			case TONE_F0:   return TONE_F0 + 24;   // F0
+			case TONE_FIS0: return TONE_FIS0 + 24; // F#0
+			case TONE_G0:   return TONE_G0 + 24;   // G0
+			case TONE_GIS0: return TONE_GIS0 + 24; // G#0
+			case TONE_A0:   return TONE_A0 + 24;   // A0
+			case TONE_AIS0: return TONE_AIS0 + 24; // A#0
+			case TONE_H0:   return TONE_H0 + 24;   // B0
+			case TONE_C1:   return TONE_C1 + 24;   // C1
+			case TONE_CIS1: return TONE_CIS1 + 24; // C#1
+			case TONE_D1:   return TONE_D1 + 24;   // D1
+			case TONE_DIS1: return TONE_DIS1 + 24; // D#1
+			case TONE_E1:   return TONE_E1 + 24;   // E1
+			case TONE_F1:   return TONE_F1 + 24;   // F1
+			case TONE_FIS1: return TONE_FIS1 + 24; // F#1
+			case TONE_G1:   return TONE_G1 + 24;   // G1
+			case TONE_GIS1: return TONE_GIS1 + 24; // G#1
+			case TONE_A1:   return TONE_A1 + 24;   // A1
+			case TONE_AIS1: return TONE_AIS1 + 24; // A#1
+			case TONE_H1:   return TONE_H1 + 24;   // B1
+			case TONE_C2:   return TONE_C2 + 24;   // C2
+			case TONE_CIS2: return TONE_CIS2 + 24; // C#2
+			case TONE_D2:   return TONE_D2 + 24;   // D2
+			case TONE_DIS2: return TONE_DIS2 + 24; // D#2
+			case TONE_E2:   return TONE_E2 + 24;   // E2
+			case TONE_F2:   return TONE_F2 + 24;   // F2
+			case TONE_FIS2: return TONE_FIS2 + 24; // F#2
+			case TONE_G2:   return TONE_G2 + 24;   // G2
+			case TONE_GIS2: return TONE_GIS2 + 24; // G#2
+			case TONE_A2:   return TONE_A2 + 24;   // A2
+			case TONE_AIS2: return TONE_AIS2 + 24; // A#2
+			case TONE_H2:   return TONE_H2 + 24;   // B2
+			case TONE_C3:   return TONE_C3 + 24;   // C3
+			case TONE_CIS3: return TONE_CIS3 + 24; // C#3
+			case TONE_D3:   return TONE_D3 + 24;   // D3
+			case TONE_DIS3: return TONE_DIS3 + 24; // D#3
+			case TONE_E3:   return TONE_E3 + 24;   // E3
+			case TONE_F3:   return TONE_F3 + 24;   // F3
+			case TONE_FIS3: return TONE_FIS3 + 24; // F#3
+			case TONE_G3:   return TONE_G3 + 24;   // G3
+			case TONE_GIS3: return TONE_GIS3 + 24; // G#3
+			case TONE_A3:   return TONE_A3 + 24;   // A3
+			case TONE_AIS3: return TONE_AIS3 + 24; // A#3
+			case TONE_H3:   return TONE_H3 + 24;   // B3
+			case TONE_C4:   return TONE_C4 + 24;   // C4
+			case TONE_CIS4: return TONE_CIS4 + 24; // C#4
+			case TONE_D4:   return TONE_D4 + 24;   // D4
+			case TONE_DIS4: return TONE_DIS4 + 24; // D#4
+			case TONE_E4:   return TONE_E4 + 24;   // E4
+			case TONE_F4:   return TONE_F4 + 24;   // F4
+			case TONE_FIS4: return TONE_FIS4 + 24; // F#4
+			case TONE_G4:   return TONE_G4 + 24;   // G4
+			case TONE_GIS4: return TONE_GIS4 + 24; // G#4
+			case TONE_A4:   return TONE_A4 + 24;   // A4
+			case NO_TONE:                          // no tone (silence)
+			default: return 0;                     // unknown tone will also return silence instead of an error.
 		}
 	}
 
@@ -393,9 +401,19 @@ public class MelodyComposer
         try 
 		{ 
 			curMelody.bpm = bpm; 
-			tmpSequence = new Sequence(Sequence.PPQ, 60000 / bpm);
+			tmpSequence = new Sequence(Sequence.PPQ, 24); // This PPQ value for siemens is assumed to be the same as Nokia's OTA/OTT (which is correct in playback speed). Tested in AH-1 SeaBomber which is available for both
 			tmpTrack = tmpSequence.createTrack();
 			tmpTrack.add(new MidiEvent(new ShortMessage(ShortMessage.PROGRAM_CHANGE, 0, 80, 0), 0)); // 80 is the Square Wave / Lead 1 instrument, which we'll use to get closer to what this should sound like
+
+			int microsecondsPerBeat = 60000000 / bpm;
+			MetaMessage metaMessage = new MetaMessage();
+			metaMessage.setMessage(0x51, new byte[] 
+			{
+				(byte) (microsecondsPerBeat >> 16),
+				(byte) (microsecondsPerBeat >> 8),
+				(byte) (microsecondsPerBeat)
+			}, 3);
+			tmpTrack.add(new MidiEvent(metaMessage, curTick)); // Add BPM change event at the current tick pos
 		} 
 		catch (Exception e) { Mobile.log(Mobile.LOG_ERROR, MelodyComposer.class.getPackage().getName() + "." + MelodyComposer.class.getSimpleName() + ": " + " couldn't create new Melody with BPM:" + e.getMessage()); }
 	}
